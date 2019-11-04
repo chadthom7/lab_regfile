82bceb002fc8197d75d268762c6b50ab040207bd
chadthom7 <chadthom7@gmail.com>
2019-11-04 15:01:45 -0500

    reorganized vectors and added another push and addition. added comments to cnt6.sv

--- cnt6.sv
+++ cnt6.sv
@@ -1,6 +1,7 @@
 	// Module to make counter
 module cnt6(input clk, rst, en_up, en_down, output logic [5:0] cnt);
-	
+	// en_up is push
+	// en_down is pop
 	// Logic to control the counter 
 	always @(posedge clk, posedge rst) begin // <--- Should we remove "posedge rst" ?
 		if (rst) cnt <= 0;
